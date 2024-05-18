c67be970451108f06c56e5d863e6be9097a77636
copypasteninja <abdugofir00@gmail.com>
2024-05-18 13:01:24 -0500

    session-3 is added

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,37 +1,41 @@
 resource "aws_instance" "web" {
-  ami           = "ami-01cd4de4363ab6ee8"  // Data Type is String, Hard Coded
+  count = 3
+  ami           = "ami-01cd4de4363ab6ee8" 
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.web.id ]   // Dynamic 
+  vpc_security_group_ids = [ aws_security_group.web.id ]  
 
   tags = {
-    Name = "terraform-session-instance-new"
+    Name = "${var.env}-instance",        
+    Environment = var.env
   }
 }
 
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+# count = meta argument, to create identical resources
+# index = 0, 1, 2, 3, 4
+# indices = plural of index (indexes)
 
-  tags = {
-    Name = "web"
-  }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.web.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
+# [ banana, apple, grape, orange ]
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.web.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# banana = 0
+# apple = 1
+# grape = 2
+# orane = 3 
 
-// How to Reference to Resource?
-// In order to Reference to Resource, we use labels (first and second)
+# [ 1, 2 ,5 , 6 ,7 ,8, 54, 56, 76 ]
+# 56 = 7
+# 6 = 3
+# 2 = 1
+# 5 = 2
 
-// Syntax: first_label.second_label.attribute
+
+
+# How to Reference to Input Variable?
+# Syntax: var.variable_name
+
+# Highlight + Command + d
+
+
+# Terraform Version:
+# 0.0.0 - 0.11.9 = ${aws_security_group.web.id}, ${var.env}
+# 0.12.0 - 1.8.5 = aws_security_group.web.id, var.env
