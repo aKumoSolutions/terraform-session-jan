b37667093431a5226d11639818944c6c09398ae2
copypasteninja <abdugofir00@gmail.com>
2024-05-29 21:42:22 -0500

    session-6 is added

--- session-6/main.tf
+++ session-6/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "github.com/aKumoSolutions/terraform-session-jan-2024//modules/ec2" 
+  source = "github.com/aKumoSolutions/terraform-session-jan-2024//modules/ec2?ref=v1.0.0" 
 
   env = "dev"
   ami = "ami-01cd4de4363ab6ee8"
@@ -8,7 +8,7 @@ module "ec2" {
 }
 
 module "sg" {
-  source = "github.com/aKumoSolutions/terraform-session-jan-2024//modules/sg"
+  source = "github.com/aKumoSolutions/terraform-session-jan-2024//modules/sg?ref=v1.0.1"
 
   env = "dev"
   ports = [ "22", "80", "443" ]
